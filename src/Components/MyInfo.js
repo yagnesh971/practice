326bc615e51204f2b2429b4523d2302de9645eab
Yagnesh Vullam <yagneshvullam95@gmail.com>
2020-07-13 03:36:34 -0400

    arrow function

--- src/Components/MyInfo.js
+++ src/Components/MyInfo.js
@@ -3,21 +3,19 @@ import Footer from './Footer';
 import NavBar from './NavBar';
 import MainContent from './MainContent';
 import Checklist from './Checklist';
-function MyInfo() {
-    return (
-        <div className="main-container">
-            <h1>Yagnesh Vullam</h1>
-            <p>This is a paragraph about me..</p>
-            <ul>
-                <li>India</li>
-                <li>Canada</li>
-                <li>USA</li>
-            </ul>
-            <NavBar className="nav-container" />
-            <MainContent className="content-container" />
-            <Footer className="footer-container" />
-            <Checklist className="checklist-container" />
-        </div>
-    )
-}
+const MyInfo = () =>
+    <div className="main-container">
+        <h1>Yagnesh Vullam</h1>
+        <p>This is a paragraph about me..</p>
+        <ul>
+            <li>India</li>
+            <li>Canada</li>
+            <li>USA</li>
+        </ul>
+        <NavBar className="nav-container" />
+        <MainContent className="content-container" />
+        <Footer className="footer-container" />
+        <Checklist className="checklist-container" />
+    </div>
+
 export default MyInfo;
