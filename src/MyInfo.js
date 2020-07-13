dad67b45f637624fd632916a6da8703ac9868fdb
Yagnesh Vullam <yagneshvullam95@gmail.com>
2020-07-13 02:17:18 -0400

    footer

--- src/MyInfo.js
+++ src/MyInfo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Footer from './Footer';
 function MyInfo() {
     return (
         <div>
@@ -9,6 +10,7 @@ function MyInfo() {
                 <li>Canada</li>
                 <li>USA</li>
             </ul>
+            <Footer />
         </div>
     )
 }
